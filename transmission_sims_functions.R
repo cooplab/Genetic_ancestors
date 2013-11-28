4633a2a27d773ff6c3d20c8a042dad7e0877a862
cooplab <gcoop79@gmail.com>
2013-11-28 10:06:27 -0800

    added the simulation scripts for transmissions

---
+++ transmission_sims_functions.R
@@ -0,0 +1,214 @@
+generate.meiotic.path<-function(num.meioses=15,only.1.sex=FALSE){	
+
+
+	if(only.1.sex %in% c("M","F")){ 	
+		my.path<-sample(inds.sex[grep(only.1.sex,inds.sex)],num.meioses)
+		}else{
+		my.path<-sample(inds.sex,num.meioses)
+	}
+	
+	chunks<-sapply(1:22,function(chr){
+		chunks<-vector("list",num.meioses)
+
+		chr.length<-max(recoms$mid[recoms$chr==chr])
+		my.chunks<-data.frame(l=0,r=chr.length+1)
+		
+	
+		for(meiosis in 1:num.meioses){
+			chunks[[meiosis]]<-my.chunks
+
+		#	print(my.chunks)
+			new.chunks<-break.chrom(ind.sex=my.path[meiosis],chr=chr,my.chunks)
+			if(length(new.chunks)==0){break;}
+			my.chunks<-new.chunks
+			stopifnot(ncol(new.chunks)==2 | ncol(new.chunks)==0)
+		#	cat("my new chunks\n")
+		#	print(new.chunks)
+		#recover()
+		}	
+	chunks
+	})
+}
+
+initiate.chrs<-function(){
+my.chunks.all.chr<-sapply(1:22,function(chr){
+	chr.length<-max(recoms$mid[recoms$chr==chr])
+	my.chunks<-data.frame(l=0,r=chr.length+1)
+	return(list(my.chunks))
+})
+my.chunks.all.chr
+}
+
+simulate.pedigree<-function(num.meioses){
+ 
+	family.chunks<-sapply(1:(num.meioses+1),function(meiosis){vector("list",2^(meiosis))})
+	family.chunks[[1]][[1]]<-initiate.chrs()
+	family.chunks[[1]][[2]]<-initiate.chrs()
+	
+	for(meiosis in 1:num.meioses){
+		par.sex<-rep(c("M","F"),2^(meiosis-1))
+		
+		for(my.par in 1:2^meiosis){
+			ind.sex<-sample(inds.sex[grep(par.sex[my.par],inds.sex)],1)
+			my.chunks.ind<-family.chunks[[meiosis]][[my.par]]
+			chunks<-lapply(1:22,function(chr){
+					my.chunks<-my.chunks.ind[[chr]]
+					if(length(my.chunks)==0) return(NULL)
+					new.chunks<-break.chrom.2(ind.sex=ind.sex,chr=chr,my.chunks)
+		#			recover()
+		#			if(length(new.chunks)==0){return("blah");}
+			#		stopifnot(ncol(new.chunks)==2 | ncol(new.chunks)==0)
+					new.chunks
+				})	
+			g.par1<-lapply(chunks,function(x){x[[1]]})
+			g.par2<-lapply(chunks,function(x){x[[2]]})
+			family.chunks[[meiosis+1]][[(my.par-1)*2+1]] <- g.par1
+			family.chunks[[meiosis+1]][[(my.par-1)*2+2]] <- g.par2
+			cat((my.par-1)*2+1,(my.par-1)*2+2," ")
+		}
+	}
+family.chunks
+}
+
+
+break.chrom.2<-function(ind.sex,chr,my.chunks){
+	stopifnot(ncol(my.chunks)==2 | ncol(my.chunks)==0)
+	pos<-recoms$mid[recoms$chr==chr & recoms$indsex==ind.sex]
+	pos<-sort(pos)
+	if(length(pos)==0){ ##no rec in chunk this gen.
+			 if(sample(0:1,1)){ 
+			 	return(list(data.frame(my.chunks),NULL))
+			 } else{
+			 	return(list(NULL,data.frame(my.chunks)))
+			 }
+	}		
+	new.chunks<-apply(my.chunks,1,function(chunk){
+		my.recs<- pos[chunk["l"] < pos & pos < chunk["r"]]
+	
+		if(length(my.recs)==0){ ##no rec in chunk this gen.
+			 if(sample(0:1,1)){ 
+			 		return(list(data.frame(l=chunk["l"], r=chunk["r"]),NULL))
+			 	} else{
+			 		return(list(NULL,data.frame(l=chunk["l"], r=chunk["r"])))
+			 	}
+			 }
+		intervals<- c(chunk["l"] ,my.recs, chunk["r"]) #break up chunk by rec
+		odds<-seq(1,length(intervals)-1,by=2)
+		evens<-seq(2,length(intervals)-1,by=2)
+		par1<-data.frame(cbind(l=intervals[odds],r=intervals[odds+1]))   ## left and right boundaries of odd segments 
+		par2<-data.frame(cbind(l=intervals[evens],r=intervals[evens+1]))  ## left and right boundaries of even segments 
+
+#				recover()
+		if(sample(0:1,1)){ 
+			return(list(par1,par2))
+		}else{
+			return(list(par2,par1))	
+		}
+	})	
+#	recover()
+
+	par1<-lapply(new.chunks,function(x){x[[1]]})
+	par2<-lapply(new.chunks,function(x){x[[2]]})
+	chunks.par1<-data.frame(l=c(unlist(sapply(par1,function(x){x$l}))),r=	c(unlist(sapply(par1,function(x){x$r}))))	
+	chunks.par2<-data.frame(l=c(unlist(sapply(par2,function(x){x$l}))),r=	c(unlist(sapply(par2,function(x){x$r}))))	
+	stopifnot(all(chunks.par1$l <= chunks.par1$r))
+	stopifnot(all(chunks.par2$l <= chunks.par2$r))
+stopifnot((ncol(chunks.par1)==2 | ncol(chunks.par1)==0) & (ncol(chunks.par2)==2 | ncol(chunks.par2)==0))
+
+	if(ncol(chunks.par1)==0) chunks.par1<-NULL
+	if(ncol(chunks.par2)==0) chunks.par2<-NULL
+	
+		
+#		cat("my new chunks in func\n")
+#	print(my.new.chunks)
+
+	return(list(chunks.par1,chunks.par2))
+	}
+
+
+break.chrom<-function(ind.sex,chr,my.chunks){
+	stopifnot(ncol(my.chunks)==2 | ncol(my.chunks)==0)
+	pos<-recoms$mid[recoms$chr==chr & recoms$indsex==ind.sex]
+	pos<-sort(pos)
+	if(length(pos)==0){ ##no rec in chunk this gen.
+			 if(sample(0:1,1)){ 
+			 	return(data.frame(my.chunks))
+			 } else{
+			 	return(NULL)
+			 }
+	}		
+	new.chunks<-apply(my.chunks,1,function(chunk){
+		my.recs<- pos[chunk["l"] < pos & pos < chunk["r"]]
+	
+		if(length(my.recs)==0){ ##no rec in chunk this gen.
+			 if(sample(0:1,1)){ 
+			 	return(data.frame(l=chunk["l"], r=chunk["r"]))
+			 	} else{
+			 		return(NULL)
+			 	}
+			 }
+		intervals<- c(chunk["l"] ,my.recs, chunk["r"]) #break up chunk by rec
+		odds<-seq(1,length(intervals)-1,by=2)
+		evens<-seq(2,length(intervals)-1,by=2)
+		par1<-data.frame(cbind(l=intervals[odds],r=intervals[odds+1]))   ## left and right boundaries of odd segments 
+		par2<-data.frame(cbind(l=intervals[evens],r=intervals[evens+1]))  ## left and right boundaries of even segments 
+#		recover()
+		if(sample(0:1,1)){ 
+			return(par1)
+			}else{
+			return(par2)	
+				}
+				##commented out text to return both sets of segments
+#		if(sample(0,1)){ 
+#			return(data.frame(cbind(par1=par1,par2=par2)))
+#		}else{
+#			return(data.frame(cbind(par1=par2,par2=par1)))	
+#		}
+	})	
+#	recover()
+
+	my.new.chunks<-data.frame(l=c(unlist(sapply(new.chunks,function(x){x$l}))),r=	c(unlist(sapply(new.chunks,function(x){x$r}))))	
+
+	stopifnot(all(my.new.chunks$l <= my.new.chunks$r))
+	stopifnot(ncol(my.new.chunks)==2 | ncol(my.new.chunks)==0)
+	
+#		cat("my new chunks in func\n")
+#	print(my.new.chunks)
+
+	return(my.new.chunks)
+	}
+	
+get.num.blocks<-function(all.chunks){
+	num.blocks.all<-sapply(1:15,function(meiosis){
+		num.blocks<-sapply(1:dim(all.chunks)[3],function(sim){
+		sum(unlist(lapply(all.chunks[meiosis,,sim],function(x){if(is.null(x)){return(0)}; nrow(x)})))
+		})
+	})
+num.blocks.all
+}
+
+get.length.blocks<-function(all.chunks){
+	blocks.length.all<-sapply(1:15,function(meiosis){
+		num.blocks<-sapply(1:dim(all.chunks)[3],function(sim){
+		sum(unlist(lapply(all.chunks[meiosis,,sim],function(x){if(is.null(x)){return(0)}; sum(x[,2]-x[,1])})))
+		})
+	})
+blocks.length.all
+}
+
+num.zeros<-function(num.blocks.all){
+sapply(1:15,function(meiosis){
+	mean(num.blocks.all[,meiosis]==0)
+	})
+}
+
+
+get.prop.zero.chr<-function(all.chunks,chr){
+	num.blocks.chr<-sapply(1:15,function(meiosis){
+		num.blocks<-sapply(1:dim(all.chunks)[3],function(sim){
+		lapply(all.chunks[meiosis,chr,sim],function(x){if(is.null(x)){return(0)}; nrow(x)})
+		})
+	})
+
+apply(num.blocks.chr,2,function(x){mean(x==0)})
+}
---
+++ transmission_simulations.R
@@ -0,0 +1,211 @@
+
+recoms<-read.table("recombination_events.out",as.is=TRUE,head=TRUE)
+source("transmission_sims_functions.R")
+
+#####Probability of transmitting all chromosomes without recombination, for mothers "M", change to "F" for fathers.
+inds<-unique(temp1$ind)
+prob.no.rec<-sapply(1:22,function(chr){
+	my.chr<-recoms[recoms$par=="M" & recoms$chr==chr,]
+	mean(!(inds %in% my.chr$ind))
+	})
+	
+	prod(prob.no.rec)
+	
+	
+	chr.transmitted<-function(ind,chr,sex,chr.length){
+	pos<-recoms$mid[recoms$chr==chr & recoms$par==sex & recoms$ind==ind]
+	intervals<-diff(c(0,sort(pos),chr.length))
+	if(length(intervals)==1) return(c(0,intervals))
+	odds<-seq(1,length(intervals),by=2)
+	evens<-seq(2,length(intervals),by=2)
+	return(c(sum(intervals[odds]),sum(intervals[evens])))
+	}
+	
+	
+###find out how much grandparental material both fathers and mothers transmit.	
+trans.male<-list()
+trans.female<-list()
+for(chr in 1:22){
+	chr.length<-max(recoms$mid[recoms$chr==chr])
+	chr.trans.male<-sapply(inds,chr.transmitted,sex="F",chr=chr,chr.length=chr.length)
+	chr.trans.female<-sapply(inds,chr.transmitted,sex="M",chr=chr,chr.length=chr.length)
+
+	trans.male[[chr]]<-chr.trans.male
+	trans.female[[chr]]<-chr.trans.female
+	}
+
+###transmission of grandparental material both fathers and mothers per chromosome
+layout(1:2)
+pdf(file="dist_transmitted_chromosome.pdf")	
+	for(chr in 1:22){
+	chr.length<-max(recoms$mid[recoms$chr==chr])
+	layout(c(1:2))
+	chr.trans.male<-trans.male[[chr]]
+	chr.trans.female<-trans.female[[chr]]
+	
+	hist(c(chr.trans.male),main=paste("male chr. ",chr),xlab="amount transmitted",breaks=seq(0,chr.length,length=20))
+	hist(c(chr.trans.female),main=paste("female chr. ",chr),xlab="amount transmitted",breaks=seq(0,chr.length,length=20))
+}
+dev.off()
+
+
+##reformat list of transmitted material per chr. per ind to be an array
+tot.trans.male<-array(dim=c(length(inds),22,2))
+tot.trans.female<-array(dim=c(length(inds),22,2))
+
+for(chr in 1:22){
+	tot.trans.female[,chr,]<-trans.female[[chr]]
+	tot.trans.male[,chr,]<-trans.male[[chr]]
+
+	}
+
+
+###transmission of grandparental material both fathers and mothers simulated for entire genome
+genome.length<-sum(sapply(1:22,function(chr){max(recoms$mid[recoms$chr==chr])}))
+pdf("fraction_of_genome_transmitted.pdf")
+layout(1:2)
+tot.genome.trans<-sapply(1:length(inds),function(ind){replicate(100,sum(sapply(1:22,function(chr){tot.trans.female[ind,chr,sample(2,1)]})))})   ##for each individual simulate whether they transmit grandmat. or grandpat material on each chr
+hist(tot.genome.trans/genome.length,main="fraction of grandparental genome transmitted via females",xlab="fraction")
+tot.genome.trans<-sapply(1:length(inds),function(ind){replicate(100,sum(sapply(1:22,function(chr){tot.trans.male[ind,chr,sample(2,1)]})))})
+hist(tot.genome.trans/genome.length,main="fraction of grandparental genome transmitted via males",xlab="fraction")
+dev.off()
+
+
+
+###generate 5000 meiotic paths of transmission 
+##this takes a while, so it is commented out.
+if(FALSE){
+all.chunks<-replicate(5000,generate.meiotic.path)
+all.chunks.fem<-replicate(5000,generate.meiotic.path(only.1.sex="M"))
+all.chunks.mal<-replicate(5000,generate.meiotic.path(only.1.sex="F"))
+save(file="my.chunks.Robj",all.chunks,all.chunks.fem,all.chunks.mal)
+}
+##you can load the Rboj instead
+load("my.chunks.Robj")
+
+	
+##count up all of the blocks inherited from a specifc ancestor
+num.blocks.all<-get.num.blocks(all.chunks)
+num.blocks.fem<-get.num.blocks(all.chunks.fem)
+num.blocks.mal<-get.num.blocks(all.chunks.mal)
+
+###Figures number of blocks inherited from a specifc ancestor
+these.meioses<- c(2,4,7,9) # 1:15
+png(file="distribution_num_blocks_egs.png")
+layout(matrix(1:4,nrow=2,byrow=TRUE))
+add.legends<-rep(TRUE,15)
+add.legends[these.meioses]<-c(rep(FALSE,3),TRUE)
+#pdf(file="~/Dropbox/Pedigree_Recom/distribution_num_blocks.pdf")
+sapply(these.meioses,function(meiosis){
+	tmp<-hist(num.blocks.mal[,meiosis],breaks=seq(-.9,50+.1,by=1),freq=FALSE,plot=FALSE)
+	plot(tmp$mids,tmp$intensities,type="l",col="blue",xlab="Number of blocks",ylab="Probability of having # blocks",main=paste(meiosis,"generations back"),lwd=2)
+	if(add.legends[meiosis]) legend("topright",c("typical transmissions","Matrilineal","Patrilineal"),col=c("black","red","blue"),lty=2,lwd=2)
+	tmp<-hist(num.blocks.all[,meiosis],breaks=seq(-.9,50+.1,by=1),freq=FALSE,plot=FALSE)
+	lines(tmp$mids,tmp$intensities,type="l",lwd=2)
+	tmp<-hist(num.blocks.fem[,meiosis],breaks=seq(-.9,50+.1,by=1),freq=FALSE,plot=FALSE)
+	lines(tmp$mids,tmp$intensities,type="l",col="red",lwd=2)
+		
+#	points(0:45-.1,dpois(0:45,(33*(meiosis-1)+22)/(2^(meiosis-1))),col="blue")
+	})
+dev.off()
+
+##compare to analytical approximation, based on poisson distribution of number of blocks
+these.meioses<-  1:15
+png(file="~/Dropbox/Pedigree_Recom/distribution_num_blocks_vs_approx.png")
+layout(matrix(1:4,nrow=2,byrow=TRUE))
+add.legends<-rep(TRUE,15)
+#add.legends[these.meioses]<-c(rep(FALSE,3),TRUE)
+pdf(file="~/Dropbox/Pedigree_Recom/distribution_num_blocks_vs_approx.pdf")
+sapply(these.meioses,function(meiosis){
+	tmp<-hist(num.blocks.all[,meiosis],breaks=seq(-.9,50+.1,by=1),freq=FALSE,plot=FALSE)
+	plot(tmp$mids,tmp$intensities,type="l",col="blue",xlab="Number of blocks",ylab="Probability of having # blocks",main=paste(meiosis,"generations back"),lwd=2)
+	if(add.legends[meiosis]) legend("topright",c("typical transmissions","Matrilineal","Patrilineal"),col=c("black","red","blue"),lty=2,lwd=2)
+
+	points(0:45-.1,dpois(0:45,(33*(meiosis-1)+22)/(2^(meiosis-1))),col="blue")
+	})
+dev.off()
+
+
+
+##sum up total genomic length of the blocks inherited from a specifc ancestor
+length.blocks.all<-get.length.blocks(all.chunks)
+length.blocks.mal<-get.length.blocks(all.chunks.mal)
+length.blocks.fem<-get.length.blocks(all.chunks.fem)
+
+##graph distribution of total genomic length of the blocks inherited from a specifc ancestor
+these.meioses<- c(2,4,7,9) # 1:15
+png(file="distribution_amount_autosomes_egs.png")
+layout(matrix(1:4,nrow=2,byrow=TRUE))
+add.legends<-rep(TRUE,15)
+add.legends[these.meioses]<-c(rep(FALSE,3),TRUE)
+#pdf(file="~/Dropbox/Pedigree_Recom/distribution_amount_autosomes.pdf")
+sapply(these.meioses,function(meiosis){
+	my.range<-range(c(length.blocks.fem[,meiosis],length.blocks.mal[,meiosis],length.blocks.all[,meiosis]) /genome.length)
+	my.breaks<-seq(my.range[1],my.range[2],length=20)
+	tmp<-hist(length.blocks.fem[,meiosis]/genome.length,freq=FALSE,plot=FALSE,breaks=my.breaks)
+	plot(tmp$mids,tmp$intensities,type="l",col="red",xlab="Fraction of autosome",ylab="Probability",main=paste(meiosis,"generations back"),lwd=2)
+	if(add.legends[meiosis]) legend("topright",c("typical transmissions","Matrilineal","Patrilineal"),col=c("black","red","blue"),lty=2,lwd=2)
+	tmp<-hist(length.blocks.mal[,meiosis]/genome.length,freq=FALSE,plot=FALSE,breaks=my.breaks)
+	lines(tmp$mids,tmp$intensities,lwd=2,col="blue")
+	tmp<-hist(length.blocks.all[,meiosis]/genome.length,freq=FALSE,plot=FALSE,breaks=my.breaks)
+	lines(tmp$mids,tmp$intensities,col="black",lwd=2)
+		
+#	points(0:45-.1,dpois(0:45,(33*(meiosis-1)+22)/(2^(meiosis-1))),col="blue")
+	})
+dev.off()
+
+###Probability of receiving zero of your genome from an ancestor k meioses back
+png(file="Prob_zero_blocks.png")
+plot(1:15,num.zeros(num.blocks.all),type="b",xlab="Generations back",ylab="Probability of inheriting zero blocks of ancestral genome")
+lines(1:15,num.zeros(num.blocks.fem),type="b",col="red")
+lines(1:15,num.zeros(num.blocks.mal),type="b",col="blue")
+legend("topleft",c("typical transmissions","Matrilineal","Patrilineal"),col=c("black","red","blue"),lty=1,lwd=2)
+dev.off()
+
+length.blocks.all<-sapply(1:10,function(meiosis){
+	num.blocks<-sapply(1:dim(all.chunks)[3],function(sim){
+	unlist(lapply(all.chunks[2,,1],function(x){x[,2]-x[,1]}))
+	})
+})
+
+
+##prob. of zero blocks inherited for each chromosome
+prop.zero.chr<-sapply(1:22,function(chr){get.prop.zero.chr(all.chunks,chr=chr)})	
+
+png(file="Prob_zero_blocks_chr.png")
+plot(c(1,15),c(0,1),type="n",ylab="Prob. of inheriting zero blocks of ancestral chromosome",
+xlab="generations back", main="Probability of inheriting zero blocks from a particular ancestor")		
+my.cols<-rainbow(22,end =0.75);sapply(1:22,function(i){lines(prop.zero.chr[,i],col=my.cols[i])})
+text(10,0.45,"chromosome")
+text(seq(8,14,length=11),rep(0.4,11),1:11,col=my.cols[1:11])
+text(seq(8,14,length=11),rep(0.35,11),12:22,col=my.cols[12:22])
+dev.off()
+
+
+#####Compare probability of zero from sims to theoretical approximation
+k<-1:15
+png(file="~/Dropbox/Pedigree_Recom/Prob_zero_blocks_vs_theory.png")
+plot(k,1-exp(-(22+35*(k-1))/(2^(k-1))),xlab="generation back",ylab="Probability that an ancestor is an autosomal genetic ancestor",type="l");points(k,1-num.zeros(num.blocks.all))
+ legend("bottomleft",legend=c("Simulations","Theoretical approximation"),pch=c(1,NA),lty=c(NA,1))
+dev.off()
+
+
+####Number of genetic ancestors vs genealogical ancestors
+png(file="~/Dropbox/Pedigree_Recom/Num_genetics_vs_genealogical_ancs.png")
+k<-1:20;num.blocks<-(22+33*(k-1))/(2^(k-1));
+
+plot(2*(1-exp(-num.blocks))*(2^(k-1)),xlab="Generations ago",ylab="Expected number of ancestors");lines(2^k,col="red")
+legend(x="topleft",legend=c("Genealogical","Genetic"),lty=c(1,NA),pch=c(NA,1),col=c("red","black"))
+dev.off()
+
+
+
+
+
+
+
+
+	
+
+		
+		
